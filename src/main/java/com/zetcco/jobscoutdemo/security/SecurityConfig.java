f4f276b362f8dffaba2e09e07f769b099d798587
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-01-30 18:24:58 +0530

    Default security overriden

---
+++ src/main/java/com/zetcco/jobscoutdemo/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.zetcco.jobscoutdemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
