6577ac5da93e44e64d8432852559f5e8f410be30
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-02 17:29:39 +0530

    File upload controller (unfinished)

---
+++ src/main/java/com/zetcco/jobscoutdemo/controllers/FileUploadController.java
@@ -0,0 +1,51 @@
+package com.zetcco.jobscoutdemo.controllers;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.HttpHeaders;
+import org.springframework.core.io.Resource;
+
+import com.zetcco.jobscoutdemo.controllers.support.ResponseMessage;
+import com.zetcco.jobscoutdemo.services.support.StorageService;
+
+@Controller
+@RequestMapping("/auth/upload")
+public class FileUploadController {
+
+    @Autowired
+    private StorageService storageService;
+
+    @PostMapping("/file")
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
+        System.out.println(file);
+        String message = "";
+        try {
+            storageService.store(file);
+            message = "Uploaded the file successfully: " + file.getOriginalFilename();
+            TimeUnit.SECONDS.sleep(3);
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+        } catch (Exception e) {
+            message = "Could not upload the file: " + file.getOriginalFilename() + ". Error: " + e.getMessage();
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+        }
+    }
+
+    @GetMapping("/file/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        Resource file = storageService.load(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+}
