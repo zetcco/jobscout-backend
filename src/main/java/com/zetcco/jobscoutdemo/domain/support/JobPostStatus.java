6a5cebafec14fefd4204623b6fc9bbd98dae7530
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-08 13:29:58 +0530

    Add skills, JobPost entities

---
+++ src/main/java/com/zetcco/jobscoutdemo/domain/support/JobPostStatus.java
@@ -0,0 +1,7 @@
+package com.zetcco.jobscoutdemo.domain.support;
+
+public enum JobPostStatus {
+    STATUS_HOLD,
+    STATUS_OVER,
+    STATUS_ACTIVE
+}
---
+++ src/main/java/com/zetcco/jobscoutdemo/domain/support/JobPostType.java
@@ -0,0 +1,7 @@
+package com.zetcco.jobscoutdemo.domain.support;
+
+public enum JobPostType {
+    TYPE_FREELANCE,
+    TYPE_PERMANENT,
+    TYPE_PART_TIME
+}
