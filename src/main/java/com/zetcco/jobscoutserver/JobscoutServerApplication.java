9c7a24a23c3661222184895c3ee6a590992ea19d
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-11 12:37:24 +0530

    Renamed main file name

--- src/main/java/com/zetcco/jobscoutserver/JobscoutServerApplication.java
+++ src/main/java/com/zetcco/jobscoutserver/JobscoutServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JobscoutDemoApplication {
+public class JobscoutServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JobscoutDemoApplication.class, args);
+		SpringApplication.run(JobscoutServerApplication.class, args);
 	}
 
 }
