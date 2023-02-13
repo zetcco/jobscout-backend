6c8e147112571c33a0bdddd2a72410a6c675c78f
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-13 13:56:12 +0530

    Add CORS filter

--- src/main/java/com/zetcco/jobscoutserver/configs/security/SecurityConfig.java
+++ src/main/java/com/zetcco/jobscoutserver/configs/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
@@ -18,11 +19,13 @@ public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationProvider authenticationProvider;
+    private final CorsFilter corsFilter;
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors();
+        // http.cors();
         http.csrf().disable();
+        http.addFilterBefore(corsFilter, ChannelProcessingFilter.class);
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         http.authorizeHttpRequests().requestMatchers(
             "/auth/**",
@@ -35,4 +38,5 @@ public class SecurityConfig {
         http.authenticationProvider(authenticationProvider);
         return http.build();
     }
+
 }
