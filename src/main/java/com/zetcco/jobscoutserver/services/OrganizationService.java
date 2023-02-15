b53e8233f49b8e630f09d2512a26d9d680432152
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-15 10:40:17 +0530

    Changed OrganizationService to return ProfileDTO

--- src/main/java/com/zetcco/jobscoutserver/services/OrganizationService.java
+++ src/main/java/com/zetcco/jobscoutserver/services/OrganizationService.java
@@ -1,12 +1,15 @@
 package com.zetcco.jobscoutserver.services;
 
+import java.util.LinkedList;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.zetcco.jobscoutserver.controllers.support.ProfileDTO;
 import com.zetcco.jobscoutserver.domain.Organization;
 import com.zetcco.jobscoutserver.repositories.OrganizationRepository;
 
@@ -16,9 +19,16 @@ public class OrganizationService {
     @Autowired
     private OrganizationRepository organizationRepository;
 
-    public List<Organization> searchOrganizationsByName(String name, int pageCount, int pageSize) {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<ProfileDTO> searchOrganizationsByName(String name, int pageCount, int pageSize) {
         String keywords = name.replace(' ', '&');
         Pageable page = PageRequest.of(pageCount, pageSize);
-        return organizationRepository.findOrganizationByNameFTS(keywords, page).getContent();
+        List<Organization> organizations = organizationRepository.findOrganizationByNameFTS(keywords, page).getContent();
+        List<ProfileDTO> profiles = new LinkedList<ProfileDTO>();
+        for (Organization organization : organizations) 
+            profiles.add(modelMapper.map(organization, ProfileDTO.class));
+        return profiles; 
     }
 }
