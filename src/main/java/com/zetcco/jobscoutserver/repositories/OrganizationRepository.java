18408e3e7ccc8931e050dfbbb04ccba2e272a874
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-15 10:03:58 +0530

    Fulltext function for Organization repo

--- src/main/java/com/zetcco/jobscoutserver/repositories/OrganizationRepository.java
+++ src/main/java/com/zetcco/jobscoutserver/repositories/OrganizationRepository.java
@@ -1,8 +1,21 @@
 package com.zetcco.jobscoutserver.repositories;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
+
 import com.zetcco.jobscoutserver.domain.Organization;
 import com.zetcco.jobscoutserver.repositories.support.UserBaseRepository;
 
 public interface OrganizationRepository extends UserBaseRepository<Organization> {
-    
+
+    @Query(value = """
+            select *
+            from organization o1_0 join _user o1_1 on o1_0.id=o1_1.id where 
+            to_tsvector(o1_0.company_name) @@ to_tsquery(:keyword)""",
+            nativeQuery = true)
+    public Page<Organization> findOrganizationByNameFTS(@Param("keyword") String keyword, Pageable page);
+
 }
