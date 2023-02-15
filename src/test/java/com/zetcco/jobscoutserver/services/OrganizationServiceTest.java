32761b4871542d60372ebc7ab7b6a23eda4ce36a
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-15 17:56:32 +0530

    Test service layer to Find org by searching name (using LIKE (%) operator)

--- src/test/java/com/zetcco/jobscoutserver/services/OrganizationServiceTest.java
+++ src/test/java/com/zetcco/jobscoutserver/services/OrganizationServiceTest.java
@@ -11,9 +11,15 @@ public class OrganizationServiceTest {
     private OrganizationService organizationService;
 
     @Test
+    void testSearchOrganizationsByNameFTS() {
+        System.out.println(organizationService.searchOrganizationsByNameFTS("not creative", 0, 1));
+        System.out.println(organizationService.searchOrganizationsByNameFTS("not creative", 1, 1));
+    }
+
+    @Test
     void testSearchOrganizationsByName() {
         System.out.println(organizationService.searchOrganizationsByName("not creative", 0, 1));
-        System.out.println(organizationService.searchOrganizationsByName("not creative", 1, 1));
+        // System.out.println(organizationService.searchOrganizationsByName("creative", 1, 1));
     }
 
 }
