2b7222a9744c768799925fd964cc13510fc54460
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-17 19:42:23 +0530

    Controller class for adding a job creator to organization

--- src/main/java/com/zetcco/jobscoutserver/controllers/OrganizationController.java
+++ src/main/java/com/zetcco/jobscoutserver/controllers/OrganizationController.java
@@ -1,12 +1,16 @@
 package com.zetcco.jobscoutserver.controllers;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
@@ -28,6 +32,19 @@ public class OrganizationController {
             return new ResponseEntity<List<ProfileDTO>>(organizationService.searchOrganizationsByName(keywords, pageCount, pageSize), HttpStatus.OK);
         } catch (IllegalArgumentException e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+
+    // TODO: Add authorization
+    @PostMapping("{organizationId}/add-creator")
+    public ResponseEntity<ProfileDTO> addCreatorToOrganization(@PathVariable("organizationId") Long organizationId, @RequestBody Map<String, Long> request) {
+        try {
+            Long jobCreatorId = request.get("id");
+            System.out.println(jobCreatorId);
+            System.out.println(organizationId);
+            return new ResponseEntity<ProfileDTO>(organizationService.addJobCreatorToOrganization(organizationId, jobCreatorId), HttpStatus.OK);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
