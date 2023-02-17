db719df59d3d6e0b6a3dae08564d3dc51fa4f453
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-17 19:39:03 +0530

    Enabled method security

--- src/main/java/com/zetcco/jobscoutserver/configs/security/SecurityConfig.java
+++ src/main/java/com/zetcco/jobscoutserver/configs/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.zetcco.jobscoutserver.configs.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
