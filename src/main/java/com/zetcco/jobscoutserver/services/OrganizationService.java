4045ca7b8f2970881bd45c1242bfbe51a4d6a248
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-17 19:42:05 +0530

    Organization Service method to add a job creators the worker list

--- src/main/java/com/zetcco/jobscoutserver/services/OrganizationService.java
+++ src/main/java/com/zetcco/jobscoutserver/services/OrganizationService.java
@@ -7,11 +7,16 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import com.zetcco.jobscoutserver.controllers.support.ProfileDTO;
+import com.zetcco.jobscoutserver.domain.JobCreator;
 import com.zetcco.jobscoutserver.domain.Organization;
+import com.zetcco.jobscoutserver.repositories.JobCreatorRepository;
 import com.zetcco.jobscoutserver.repositories.OrganizationRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class OrganizationService {
@@ -21,6 +26,9 @@ public class OrganizationService {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private JobCreatorRepository jobCreatorRepository;
 
     @Autowired
     private ModelMapper modelMapper;
@@ -44,5 +52,21 @@ public class OrganizationService {
         for (Organization organization : organizations) 
             profiles.add(modelMapper.map(organization, ProfileDTO.class));
         return profiles; 
+    }
+
+    @PreAuthorize("hasRole('ORGANIZATION')")
+    @Transactional
+    public ProfileDTO addJobCreatorToOrganization(Long organizationId, Long jobCreatorId) {
+        Organization organization = organizationRepository.findById(organizationId).orElseThrow();
+        List<JobCreator> jobCreators = organization.getJobCreators();
+        jobCreators.add(jobCreatorRepository.findById(jobCreatorId).orElseThrow());
+        for (JobCreator jobCreator : jobCreators) 
+            jobCreator.setOrganization(organization);
+        organization.setJobCreators(jobCreators);
+
+        jobCreatorRepository.saveAll(jobCreators);
+        organizationRepository.save(organization);
+
+        return userService.getUser(organizationId);
     }
 }
