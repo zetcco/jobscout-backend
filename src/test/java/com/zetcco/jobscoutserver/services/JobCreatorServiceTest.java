df0aaa8e8d6966ae79d75b660b38b4d9c026e68a
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-18 11:14:54 +0530

    Test class for JobCreator service

---
+++ src/test/java/com/zetcco/jobscoutserver/services/JobCreatorServiceTest.java
@@ -0,0 +1,18 @@
+package com.zetcco.jobscoutserver.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JobCreatorServiceTest {
+
+    @Autowired
+    private JobCreatorService jobCreatorService;
+
+    @Test
+    void testRequestForOrganization() {
+        System.out.println(jobCreatorService.requestForOrganization(107L, 75L));
+    }
+
+}
