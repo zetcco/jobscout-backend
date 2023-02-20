196c5556711b813c670ba0963c50afc85dac5783
ANMSilva <nipunmadumal18@gmail.com>
2023-02-20 15:30:53 +0530

    Unused imported components deleted in Category.java

--- src/main/java/com/zetcco/jobscoutserver/domain/Category.java
+++ src/main/java/com/zetcco/jobscoutserver/domain/Category.java
@@ -4,12 +4,10 @@ import java.util.List;
 
 import com.zetcco.jobscoutserver.domain.support.User;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
