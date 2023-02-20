1e2a50cd2282db07d57fa3fabbf2ed9f4025bf60
ANMSilva <nipunmadumal18@gmail.com>
2023-02-21 00:50:42 +0530

    CategoryRepository.java created

---
+++ src/test/java/com/zetcco/jobscoutserver/repositories/CategoryRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.zetcco.jobscoutserver.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.zetcco.jobscoutserver.domain.Category;
+
+@SpringBootTest
+public class CategoryRepositoryTest {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    private void SaveCategory(){
+        Category category = Category
+                                
+    }
+    
+}
