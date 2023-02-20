a4a974878322e9c509478a1a900060b9d3caeb05
ANMSilva <nipunmadumal18@gmail.com>
2023-02-21 01:54:21 +0530

    Skill.java modified

--- src/main/java/com/zetcco/jobscoutserver/domain/Skill.java
+++ src/main/java/com/zetcco/jobscoutserver/domain/Skill.java
@@ -5,13 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
+@Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Skill {
