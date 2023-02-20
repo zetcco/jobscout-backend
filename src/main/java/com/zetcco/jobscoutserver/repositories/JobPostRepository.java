2cd1c9659100014043834993d1052fc7af894548
ANMSilva <nipunmadumal18@gmail.com>
2023-02-20 14:47:00 +0530

    JobPostRepository.java created

---
+++ src/main/java/com/zetcco/jobscoutserver/repositories/JobPostRepository.java
@@ -0,0 +1,7 @@
+package com.zetcco.jobscoutserver.repositories;
+
+import com.zetcco.jobscoutserver.domain.support.User;
+import com.zetcco.jobscoutserver.repositories.support.UserBaseRepository;
+public interface JobPostRepository extends UserBaseRepository<User>{
+    
+}
