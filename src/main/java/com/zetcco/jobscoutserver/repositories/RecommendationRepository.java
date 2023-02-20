23cfc359fe89a46074df6e2dd0ef300a540f55bb
Gayesh Chamoda <npgayesh@gmail.com>
2023-02-21 01:29:03 +0530

    change recommendation repository

---
+++ src/main/java/com/zetcco/jobscoutserver/repositories/RecommendationRepository.java
@@ -0,0 +1,10 @@
+package com.zetcco.jobscoutserver.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.zetcco.jobscoutserver.domain.Recommendation;
+
+@Repository
+public interface RecommendationRepository extends JpaRepository<Recommendation, Long>{
+    
+}
