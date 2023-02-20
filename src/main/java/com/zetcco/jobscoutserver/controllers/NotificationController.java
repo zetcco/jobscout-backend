dd15b4713abbe36b5c8d1c74e887e0bd8e6c1cab
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-20 21:11:03 +0530

    Fetch notifications controller

--- src/main/java/com/zetcco/jobscoutserver/controllers/NotificationController.java
+++ src/main/java/com/zetcco/jobscoutserver/controllers/NotificationController.java
@@ -3,11 +3,13 @@ package com.zetcco.jobscoutserver.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.zetcco.jobscoutserver.services.NotificationService;
 import com.zetcco.jobscoutserver.services.support.NotificationDTO;
@@ -19,13 +21,13 @@ public class NotificationController {
     @Autowired
     private NotificationService notificationService;
 
-    // @GetMapping("/{id}")
-    // public ResponseEntity<List<NotificationDTO>> getNotifications(@PathVariable("id") Long id) {
-    //     try {
-    //         // notificationService.get
-    //     } catch (Exception e) {
-
-    //     }
-    // }
+    @GetMapping()
+    public ResponseEntity<List<NotificationDTO>> getNotifications(@RequestParam("limit") int pageSize, @RequestParam("offset") int pageCount) {
+        try {
+            return new ResponseEntity<List<NotificationDTO>>(notificationService.getNotificationsForCurrentUser(pageCount, pageSize), HttpStatus.OK);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
 
 }
