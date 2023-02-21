6d0be1e44f33804b683edb3476920834f73e6cc5
ANMSilva <nipunmadumal18@gmail.com>
2023-02-21 14:08:21 +0530

    CategoryRepositoryTest.java modifed and Tested

--- src/test/java/com/zetcco/jobscoutserver/repositories/CategoryRepositoryTest.java
+++ src/test/java/com/zetcco/jobscoutserver/repositories/CategoryRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.zetcco.jobscoutserver.repositories;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,13 +14,28 @@ public class CategoryRepositoryTest {
     private CategoryRepository categoryRepository;
 
     @Test
-    public void saveCategory(){
+    public void addCategory(){
         Category category = Category.builder()
-                                    .name("Software Engineer")
+                                    .name("Software Engineering")
                                     .description("Senior Software Engineer")
                                     .build();
-        categoryRepository.save(category);   
-                                
+        categoryRepository.save(category);                              
     }
+
+    @Test
+    public void getAllCategories() throws Exception{
+            List<Category> category =  categoryRepository.findAll();
+            category.forEach((p)->{
+                System.out.println(p.getName());
+            }); 
+    }
+
+    @Test
+    public void getCategoryByName(){
+            List<Category> category = categoryRepository.findByNameIgnoreCase("Software Engineer");
+            category.forEach((p)->{
+                System.out.println(p.getName());
+            });
+    }        
     
 }
