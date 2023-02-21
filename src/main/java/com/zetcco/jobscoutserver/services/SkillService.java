7bcd99c9e0623dd274cf2c2c4c9d24a56bce90f2
Thanis <77865201+thanis04@users.noreply.github.com>
2023-02-21 15:59:30 +0530

    skill service work properly now

--- src/main/java/com/zetcco/jobscoutserver/services/SkillService.java
+++ src/main/java/com/zetcco/jobscoutserver/services/SkillService.java
@@ -9,56 +9,32 @@ import org.springframework.stereotype.Service;
 
 import com.zetcco.jobscoutserver.domain.Skill;
 import com.zetcco.jobscoutserver.repositories.SkillsRepository;
-
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class SkillService {
 
-
     @Autowired
     private SkillsRepository skillsRepository;
 
-    
-    // public List<ProfileDTO> searchOrganizationsByName(String name, int pageCount, int pageSize) {
-    //     if (name.equals(""))
-    //         throw new IllegalArgumentException("Wrong Parameters");
-    //     Pageable page = PageRequest.of(pageCount, pageSize);
-    //     List<Organization> organizations = organizationRepository.findByCompanyNameContainingIgnoreCase(name, page).getContent();
-    //     List<ProfileDTO> profiles = new LinkedList<ProfileDTO>();
-    //     for (Organization organization : organizations) 
-    //         profiles.add(userService.getUser(organization.getId()));
-    //     return profiles; 
+    // public List<Skill> searchSkillByName(String name, int pageCount, int
+    // pageSize) {
+    // if (name.equals(""))
+    // throw new IllegalArgumentException("Wrong Parameters");
+    // // Pageable page = PageRequest.of(pageCount, pageSize);
+    // List<Skill> skills = skillsRepository.findByNameContainingIgnoreCase(name);
+    // return skills;
     // }
 
-    // public List<ProfileDTO> searchOrganizationsByNameFTS(String name, int pageCount, int pageSize) {
-    //     String keywords = name.replace(' ', '&');
-    //     Pageable page = PageRequest.of(pageCount, pageSize);
-    //     List<Organization> organizations = organizationRepository.findOrganizationByNameFTS(keywords, page).getContent();
-    //     List<ProfileDTO> profiles = new LinkedList<ProfileDTO>();
-    //     for (Organization organization : organizations) 
-    //         profiles.add(modelMapper.map(organization, ProfileDTO.class));  
-    //     return profiles; 
-    // }
-
-    public List<Skill> searchSkillByName(String name, int pageCount, int pageSize) {
-        if(name.equals(""))
+    public List<Skill> searchSkillByName(String name) {
+        if (name.equals(""))
             throw new IllegalArgumentException("Wrong Parameters");
-        // Pageable page = PageRequest.of(pageCount, pageSize);
         List<Skill> skills = skillsRepository.findByNameContainingIgnoreCase(name);
-        return null;
+        return skills;
     }
 
-    @Transactional
-    List<Skill> addSkills(Long skillId) throws NotFoundException, DataIntegrityViolationException {
-        Skill skill = this.getSkillsById(skillId);
-
-        return null;
-    }
-
-    Skill getSkillsById(Long skillId) throws NotFoundException {
-        return skillsRepository.findById(skillId).orElseThrow(() -> new NotFoundException());
+    Skill addSkills(Skill skill) throws NotFoundException, DataIntegrityViolationException {
+        return skillsRepository.save(skill);
     }
 }
