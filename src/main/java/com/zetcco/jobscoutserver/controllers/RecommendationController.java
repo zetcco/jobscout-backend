76a5e38618fe3a4656888dbf815f5549e51c5c0d
Gayesh Chamoda <npgayesh@gmail.com>
2023-02-22 00:47:59 +0530

    change recommendation controller

--- src/main/java/com/zetcco/jobscoutserver/controllers/RecommendationController.java
+++ src/main/java/com/zetcco/jobscoutserver/controllers/RecommendationController.java
@@ -1,5 +1,25 @@
 package com.zetcco.jobscoutserver.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zetcco.jobscoutserver.domain.Recommendation;
+import com.zetcco.jobscoutserver.repositories.RecommendationRepository;
+
+@RestController
+@RequestMapping("/recommendation")
 public class RecommendationController {
     
+    @Autowired
+    private RecommendationRepository recommendationRepository;
+
+    @GetMapping("/all")
+    private List<Recommendation> gRecommendations() {
+        return null;
+
+    } 
 }
