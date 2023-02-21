bf9e390044d3de8a944444a278431158df8695eb
Gayesh Chamoda <npgayesh@gmail.com>
2023-02-21 17:01:40 +0530

    correcting errors

--- src/main/java/com/zetcco/jobscoutserver/repositories/RecommendationRepository.java
+++ src/main/java/com/zetcco/jobscoutserver/repositories/RecommendationRepository.java
@@ -1,10 +1,9 @@
 package com.zetcco.jobscoutserver.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.zetcco.jobscoutserver.domain.Recommendation;
 
-@Repository
+
 public interface RecommendationRepository extends JpaRepository<Recommendation, Long>{
     
 }
