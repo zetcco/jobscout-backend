3b601221df9ddc6fa261ff9b0ecfdb6fb490d92c
Thanis <77865201+thanis04@users.noreply.github.com>
2023-02-21 19:35:20 +0530

    addSkills mwthod created

--- src/main/java/com/zetcco/jobscoutserver/controllers/SkillController.java
+++ src/main/java/com/zetcco/jobscoutserver/controllers/SkillController.java
@@ -3,10 +3,14 @@ package com.zetcco.jobscoutserver.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
@@ -30,6 +34,11 @@ public class SkillController {
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
+    }
+
+    @PostMapping("/add-skills")
+    public ResponseEntity<Skill> addSkills(@RequestBody Skill skill) throws DataIntegrityViolationException, NotFoundException {
+        return new ResponseEntity<Skill>(skillService.addSkills(skill), HttpStatus.OK);
     }
     
 }
