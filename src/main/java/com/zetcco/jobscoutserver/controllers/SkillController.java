9b986d5a1352bbf7ba691b6d6724975e6544d359
Thanis <77865201+thanis04@users.noreply.github.com>
2023-02-21 16:50:04 +0530

    searchSkills done

--- src/main/java/com/zetcco/jobscoutserver/controllers/SkillController.java
+++ src/main/java/com/zetcco/jobscoutserver/controllers/SkillController.java
@@ -1,5 +1,35 @@
 package com.zetcco.jobscoutserver.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.zetcco.jobscoutserver.domain.Skill;
+import com.zetcco.jobscoutserver.services.SkillService;
+
+@Controller
+@RequestMapping("/skills")
 public class SkillController {
+
+    @Autowired
+    private SkillService skillService;
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Skill>> searchSkills(@RequestParam("search") String name) {
+        try {
+            return new ResponseEntity<List<Skill>>(skillService.searchSkillByName(name), HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
     
 }
