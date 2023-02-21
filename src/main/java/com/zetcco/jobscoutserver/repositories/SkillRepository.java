fcc096581f5de45740655de721b9bad446ce8f4a
ANMSilva <nipunmadumal18@gmail.com>
2023-02-21 18:43:11 +0530

    ServiceRepository.java modified

--- src/main/java/com/zetcco/jobscoutserver/repositories/SkillRepository.java
+++ src/main/java/com/zetcco/jobscoutserver/repositories/SkillRepository.java
@@ -3,8 +3,6 @@ package com.zetcco.jobscoutserver.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
 import com.zetcco.jobscoutserver.domain.Skill;
 
 public interface SkillRepository extends JpaRepository<Skill , Long>{
