8d5790917d8c3ead694fc9cf8233f768f076d843
ANMSilva <nipunmadumal18@gmail.com>
2023-02-21 14:10:08 +0530

    CategoryRepository. java modified

--- src/main/java/com/zetcco/jobscoutserver/repositories/CategoryRepository.java
+++ src/main/java/com/zetcco/jobscoutserver/repositories/CategoryRepository.java
@@ -1,8 +1,11 @@
 package com.zetcco.jobscoutserver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.zetcco.jobscoutserver.domain.Category;
 
 public interface CategoryRepository extends JpaRepository<Category , Long>{
-    
+
+        List<Category> findByNameIgnoreCase(String name);    
 }
