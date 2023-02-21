15ade706c036d67ab3e4761cf28b0ca2a5342b3c
ANMSilva <nipunmadumal18@gmail.com>
2023-02-21 21:35:39 +0530

    Categoryservices.java modified

--- src/main/java/com/zetcco/jobscoutserver/services/CategoryService.java
+++ src/main/java/com/zetcco/jobscoutserver/services/CategoryService.java
@@ -2,7 +2,10 @@ package com.zetcco.jobscoutserver.services;
 
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
     
 }
