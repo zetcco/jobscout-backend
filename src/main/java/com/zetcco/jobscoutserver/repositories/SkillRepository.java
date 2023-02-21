524c8143043b7cea3827d8d5c11429aa0b0b69f5
ANMSilva <nipunmadumal18@gmail.com>
2023-02-21 14:18:22 +0530

    SkillRepository.java created

---
+++ src/main/java/com/zetcco/jobscoutserver/repositories/SkillRepository.java
@@ -0,0 +1,14 @@
+package com.zetcco.jobscoutserver.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.zetcco.jobscoutserver.domain.Skill;
+
+public interface SkillRepository extends JpaRepository<Skill , Long>{
+
+    List<Skill> findByNameIgnoreCase(String name);
+
+}
