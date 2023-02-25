f857357de217b1d94934a94a925779748ca9960a
ANMSilva <nipunmadumal18@gmail.com>
2023-02-25 15:14:01 +0530

    CategoryServiceTest,java modified

--- src/test/java/com/zetcco/jobscoutserver/services/CategoryServiceTest.java
+++ src/test/java/com/zetcco/jobscoutserver/services/CategoryServiceTest.java
@@ -1,8 +1,12 @@
 package com.zetcco.jobscoutserver.services;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.zetcco.jobscoutserver.domain.Category;
 
 @SpringBootTest
 public class CategoryServiceTest {
@@ -11,6 +15,29 @@ public class CategoryServiceTest {
 
     @Test
     void testAddNewCategory(){
-        categoryService.addNewCategory("HR manager", "part time job with good salary");
+        categoryService.addNewCategory("Associate software engineer", "part time job with good salary");
+    }
+
+    @Test
+    void testGetAllCatgorie(){
+
+        List<Category> category = categoryService.getAllCategories();
+            category.forEach((p)->{
+                System.out.println(p.getName());
+            }); 
+    }
+
+    @Test 
+    void testGetCategoryByNameIgnoreCase(){
+        Category category = categoryService.getCategoryByNameIgnoreCase("HR manager");
+        System.out.println(category.getName());
+    }
+
+    @Test
+    void testGetCategoryByNameContainingIgnoreCase(){
+        List<Category> category = categoryService.getCategoryByNameContainingIgnoreCase("engineer");
+        category.forEach((p)->{
+            System.out.println(p.getName());
+        });
     }
 }
