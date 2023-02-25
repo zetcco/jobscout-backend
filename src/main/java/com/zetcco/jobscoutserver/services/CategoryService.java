ccd2efd8bd151b60681fea6d44286f0906c9f460
ANMSilva <nipunmadumal18@gmail.com>
2023-02-25 12:49:34 +0530

    CategoryService.java modified

--- src/main/java/com/zetcco/jobscoutserver/services/CategoryService.java
+++ src/main/java/com/zetcco/jobscoutserver/services/CategoryService.java
@@ -1,11 +1,30 @@
 package com.zetcco.jobscoutserver.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.zetcco.jobscoutserver.domain.Category;
+import com.zetcco.jobscoutserver.repositories.CategoryRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CategoryService {
     
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public void addNewCategory( String name , String description){
+        List<Category> category = this.categoryRepository.findByNameIgnoreCase(name);
+        if( category == null){
+            Category newCategory = new Category(null, name , description, null, null, null);
+            categoryRepository.save(newCategory);
+        }
+    }
+
 }
