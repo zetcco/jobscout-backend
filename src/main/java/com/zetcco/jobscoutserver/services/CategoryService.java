0cbbf590a5c3bb7a409ba4a58bcefa79d99b99b1
ANMSilva <nipunmadumal18@gmail.com>
2023-02-25 15:10:41 +0530

    CategoryService.java modified

--- src/main/java/com/zetcco/jobscoutserver/services/CategoryService.java
+++ src/main/java/com/zetcco/jobscoutserver/services/CategoryService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.zetcco.jobscoutserver.domain.Category;
 import com.zetcco.jobscoutserver.repositories.CategoryRepository;
+import com.zetcco.jobscoutserver.services.support.NotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,11 +18,38 @@ public class CategoryService {
     private CategoryRepository categoryRepository;
 
     public void addNewCategory( String name , String description){
-        List<Category> category = this.categoryRepository.findByNameIgnoreCase(name);
-        if( category.isEmpty() == true){
+        Category category = this.categoryRepository.findByNameIgnoreCase(name);
+        if( category == null){
             Category newCategory = new Category(null, name , description, null, null, null);
             categoryRepository.save(newCategory);
         }
     }
 
+    public List<Category> getAllCategories() throws NotFoundException {
+        List<Category> category = categoryRepository.findAll();
+        if(category.isEmpty() == true){
+            throw new NotFoundException("Categories Not Found!");
+        }else{
+            return category;
+        }
+        
+    }
+
+    public Category getCategoryByNameIgnoreCase(String name) throws NotFoundException{
+            Category category = categoryRepository.findByNameIgnoreCase(name);
+            if(category == null){
+                throw new NotFoundException("Category Not Found!");
+            }else{
+                return category;
+            }
+    }
+
+    public List<Category> getCategoryByNameContainingIgnoreCase(String name) throws NotFoundException{
+            List<Category> category =  categoryRepository.findByNameContainingIgnoreCase(name);
+            if(category.isEmpty() == true){
+                throw new NotFoundException("Categories Not Found!");
+            }else{
+                return category;
+            }
+    }
 }
