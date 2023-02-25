af1b4b448d7d0f5af5d015bfe056c6ffb18d1f5c
Gayesh Chamoda <npgayesh@gmail.com>
2023-02-25 12:22:01 +0530

    complete recommendationRepoTest

--- src/test/java/com/zetcco/jobscoutserver/repositories/RecommendationRepositoryTest.java
+++ src/test/java/com/zetcco/jobscoutserver/repositories/RecommendationRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.zetcco.jobscoutserver.repositories;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,11 +17,10 @@ public class RecommendationRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
-
     
 
     @Test
-    public void saveRecommendation() {
+    public void saveRecommendationTest1() {
         User requester = userRepository.findById(1L).orElseThrow();
         User responder = userRepository.findById(2L).orElseThrow();
 
@@ -31,5 +32,57 @@ public class RecommendationRepositoryTest {
         
         recommendationRepository.save(recommendation);
     }
+
+    @Test
+    public void saveRecommendationTest2() {
+        User requester = userRepository.findById(3L).orElseThrow();
+        User responder = userRepository.findById(4L).orElseThrow();
+
+        Recommendation recommendation = Recommendation.builder()
+                                            .content("not-recommended")
+                                            .requester(requester)
+                                            .responder(responder)
+                                            .build();
         
+        recommendationRepository.save(recommendation);
+    }
+
+    @Test
+    public void saveRecommendationTest3() {
+        User requester = userRepository.findById(3L).orElseThrow();
+        User responder = userRepository.findById(1L).orElseThrow();
+
+        Recommendation recommendation = Recommendation.builder()
+                                            .content("weakly-recommended")
+                                            .requester(requester)
+                                            .responder(responder)
+                                            .build();
+        
+        recommendationRepository.save(recommendation);
+    }
+
+
+    @Test
+    public void getRecommendationTest() {
+        Recommendation recommendation = recommendationRepository.findById(1L).orElseThrow();
+        System.out.println(recommendation);
+    }
+        
+    @Test
+    public void updateRecommendation() {
+        Recommendation recommendation = recommendationRepository.findById(2L).orElseThrow();
+        recommendation.setContent("highly recommended");
+
+        User requester = userRepository.findById(1L).orElseThrow();
+        recommendation.setRequester(requester);
+
+        recommendationRepository.save(recommendation);
+    }
+
+    @Test
+    public void deleteRecommendation() {
+        Recommendation recommendation = recommendationRepository.findById(3L).orElseThrow();
+        recommendationRepository.delete(recommendation);
+
+    }
 }
