6091cb30ec6f97cad9dab0d5ac4811eba9d98522
Gayesh Chamoda <npgayesh@gmail.com>
2023-02-26 00:41:21 +0530

    create recommendationDTO class

---
+++ src/main/java/com/zetcco/jobscoutserver/services/support/RecommendationDTO.java
@@ -0,0 +1,21 @@
+package com.zetcco.jobscoutserver.services.support;
+
+import com.zetcco.jobscoutserver.domain.support.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class RecommendationDTO {
+
+    private Long recommendationId;
+    private String content;
+    private User requester;
+    private User responder;
+
+}
