70725ca59d25523e9f8aa867a39886859bd566c0
Gayesh Chamoda <npgayesh@gmail.com>
2023-02-26 00:41:52 +0530

    change repository

--- src/main/java/com/zetcco/jobscoutserver/repositories/RecommendationRepository.java
+++ src/main/java/com/zetcco/jobscoutserver/repositories/RecommendationRepository.java
@@ -1,9 +1,17 @@
 package com.zetcco.jobscoutserver.repositories;
 
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.zetcco.jobscoutserver.domain.Recommendation;
 
 
 public interface RecommendationRepository extends JpaRepository<Recommendation, Long>{
+    
+    // @Query(value = "SELECT * FROM recommendation WHERE requester = ?1", nativeQuery = true)
+    List<Recommendation> findByRequesterId(Long requesterId);
     
 }
