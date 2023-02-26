9b2e6d05eba0bae86b0cd84e589b4b9b96100c7f
ANMSilva <nipunmadumal18@gmail.com>
2023-02-26 12:48:05 +0530

    CategoryRepository.java modified

--- src/main/java/com/zetcco/jobscoutserver/repositories/CategoryRepository.java
+++ src/main/java/com/zetcco/jobscoutserver/repositories/CategoryRepository.java
@@ -1,12 +1,14 @@
 package com.zetcco.jobscoutserver.repositories;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.zetcco.jobscoutserver.domain.Category;
 
 public interface CategoryRepository extends JpaRepository<Category , Long>{
 
-        Category findByNameIgnoreCase(String name); 
+        Optional<Category> findByNameIgnoreCase(String name); 
         
         List<Category> findByNameContainingIgnoreCase(String name);
 }
