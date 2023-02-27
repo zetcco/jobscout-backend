7b4ea4edf2b759091280ef24566f547be6e1173d
Gimhan Mahela <gimhanmahela@gmail.com>
2023-02-27 14:33:13 +0530

    create blog post repository

---
+++ src/main/java/com/zetcco/jobscoutserver/repositories/BlogPostRepository.java
@@ -0,0 +1,13 @@
+package com.zetcco.jobscoutserver.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.zetcco.jobscoutserver.domain.BlogPost;
+import com.zetcco.jobscoutserver.domain.support.User;
+
+public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
+    Optional<User> findByUser(String user);
+
+}
