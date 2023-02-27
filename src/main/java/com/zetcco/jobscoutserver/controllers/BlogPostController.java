4b890dd4b147fa1706f0e9f55874e1ccbfba4ad8
Gimhan Mahela <gimhanmahela@gmail.com>
2023-02-27 14:34:04 +0530

    ceate blog post controller

---
+++ src/main/java/com/zetcco/jobscoutserver/controllers/BlogPostController.java
@@ -0,0 +1,10 @@
+package com.zetcco.jobscoutserver.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/posts")
+public class BlogPostController {
+
+}
