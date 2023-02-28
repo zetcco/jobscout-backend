b190679334f5df771af7cc96ba26746aaa08bdbd
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-28 08:35:24 +0530

    Meeting model

---
+++ src/main/java/com/zetcco/jobscoutserver/domain/Meeting.java
@@ -0,0 +1,28 @@
+package com.zetcco.jobscoutserver.domain;
+
+import com.zetcco.jobscoutserver.domain.support.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Meeting {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User hoster;
+}
