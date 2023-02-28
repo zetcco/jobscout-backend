500fae0193e042c699fa51a6dfd5c42b9a08528e
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-28 08:38:46 +0530

    RTCService warnings cleared

--- src/main/java/com/zetcco/jobscoutserver/services/RTCService.java
+++ src/main/java/com/zetcco/jobscoutserver/services/RTCService.java
@@ -1,7 +1,7 @@
 package com.zetcco.jobscoutserver.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import com.zetcco.jobscoutserver.domain.support.RTCSignal;
@@ -9,8 +9,8 @@ import com.zetcco.jobscoutserver.domain.support.RTCSignal;
 @Service
 public class RTCService {
     
-    @Autowired
-    private SimpMessagingTemplate simpMessagingTemplate;
+    // @Autowired
+    // private SimpMessagingTemplate simpMessagingTemplate;
 
     public void sendToUser(RTCSignal signal) {
         // simpMessagingTemplate.convertAndSendToUser(signal.getRecieverId().toString(), "/call", signal);
