c89c5c987ecad1a29760d5122527fe52582f2543
Thanis <77865201+thanis04@users.noreply.github.com>
2023-03-01 09:56:19 +0530

    method named fetchSkillByCategory

--- src/main/java/com/zetcco/jobscoutserver/services/JobSeekerService.java
+++ src/main/java/com/zetcco/jobscoutserver/services/JobSeekerService.java
@@ -1,8 +1,11 @@
 package com.zetcco.jobscoutserver.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.zetcco.jobscoutserver.domain.Skill;
 import com.zetcco.jobscoutserver.repositories.CategoryRepository;
 import com.zetcco.jobscoutserver.repositories.SkillsRepository;
 
@@ -14,5 +17,9 @@ public class JobSeekerService {
 
     @Autowired
     private SkillsRepository skillsRepository;
+
+    public List<Skill> fetchSkillsByCategory() {
+        return null;
+    }
     
 }
