0842a0e50d66ebe313cd90e0cfd8385ac28e091e
Thanis <77865201+thanis04@users.noreply.github.com>
2023-03-01 11:39:02 +0530

    some part removed

--- src/main/java/com/zetcco/jobscoutserver/services/JobSeekerService.java
+++ src/main/java/com/zetcco/jobscoutserver/services/JobSeekerService.java
@@ -1,25 +1,15 @@
 package com.zetcco.jobscoutserver.services;
-
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.zetcco.jobscoutserver.domain.Category;
 import com.zetcco.jobscoutserver.domain.Skill;
-import com.zetcco.jobscoutserver.repositories.CategoryRepository;
-import com.zetcco.jobscoutserver.repositories.SkillsRepository;
+
+import jakarta.persistence.ManyToOne;
 
 @Service
 public class JobSeekerService {
 
-    @Autowired
-    private CategoryRepository categoryRepository;
-
-    @Autowired
-    private SkillsRepository skillsRepository;
-
-    public List<Skill> fetchSkillsByCategory() {
-        return null;
-    }
     
 }
