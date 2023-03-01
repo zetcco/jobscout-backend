1aba41de3ff0daa5d813c90fd9bce3f7503f3177
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-03-01 14:38:35 +0530

    DTO for Meeting

--- src/main/java/com/zetcco/jobscoutserver/domain/support/MeetingDTO.java
+++ src/main/java/com/zetcco/jobscoutserver/domain/support/MeetingDTO.java
@@ -1,13 +1,25 @@
 package com.zetcco.jobscoutserver.domain.support;
 
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.zetcco.jobscoutserver.services.support.ProfileDTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MeetingDTO {
     private Long id;
+    private String link;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private Date timestamp;  
+
     private ProfileDTO hoster;
 }
