826b27d9dd04df01dc66d853b8651eb397f400fb
Gayesh Chamoda <npgayesh@gmail.com>
2023-03-02 20:25:35 +0530

    Make Recommendation Repository and Test

--- src/main/java/com/zetcco/jobscoutserver/repositories/RecommendationRepository.java
+++ src/main/java/com/zetcco/jobscoutserver/repositories/RecommendationRepository.java
@@ -1,16 +1,11 @@
 package com.zetcco.jobscoutserver.repositories;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.zetcco.jobscoutserver.domain.Recommendation;
 
-
-public interface RecommendationRepository extends JpaRepository<Recommendation, Long>{
-    
-    // @Query(value = "SELECT * FROM recommendation WHERE requester = ?1", nativeQuery = true)
-    
-    Recommendation findByRecommendationId(Long recommendationsRecommendationId);
-
+@Repository
+public interface RecommendationRepository extends JpaRepository <Recommendation, Long> {
     
 }
