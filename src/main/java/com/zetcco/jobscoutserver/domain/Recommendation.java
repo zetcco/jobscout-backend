423dececf35b743693634894d8c5926c81e9c20a
Gayesh Chamoda <npgayesh@gmail.com>
2023-03-02 20:24:53 +0530

    Create recommendation class

--- src/main/java/com/zetcco/jobscoutserver/domain/Recommendation.java
+++ src/main/java/com/zetcco/jobscoutserver/domain/Recommendation.java
@@ -2,14 +2,19 @@ package com.zetcco.jobscoutserver.domain;
 
 import com.zetcco.jobscoutserver.domain.support.User;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+@Entity
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Recommendation {
