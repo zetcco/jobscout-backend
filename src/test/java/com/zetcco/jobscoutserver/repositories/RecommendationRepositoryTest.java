e4c5537ed5b1234f2489caf7f85805b5a2852a74
Gayesh Chamoda <npgayesh@gmail.com>
2023-03-02 20:25:35 +0530

    Make Recommendation Repository and Test

--- src/test/java/com/zetcco/jobscoutserver/repositories/RecommendationRepositoryTest.java
+++ src/test/java/com/zetcco/jobscoutserver/repositories/RecommendationRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.zetcco.jobscoutserver.repositories;
 
-
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -13,7 +12,6 @@ import com.zetcco.jobscoutserver.domain.JobSeeker;
 import com.zetcco.jobscoutserver.domain.Recommendation;
 import com.zetcco.jobscoutserver.domain.support.User;
 import com.zetcco.jobscoutserver.services.support.NotFoundException;
-
 
 @SpringBootTest
 public class RecommendationRepositoryTest {
@@ -29,118 +27,85 @@ public class RecommendationRepositoryTest {
 
     @Autowired
     private JobSeekerRepository jobSeekerRepository;
-
+    
+    
     @Test
-    public void saveRecommendationTest1() {
-        // User requester = userRepository.findById(1L).orElseThrow();
-        User responder = userRepository.findById(2L).orElseThrow();
+    public void saveRecommendation() {
+        User responder = userRepository.findById(3L).orElseThrow();
+        // User requester = userRepository.findById(2L).orElseThrow();
 
         Recommendation recommendation = Recommendation.builder()
-                                            .content("recommended")
-                                            .responder(responder)
-                                            .build();
-        
+                                                        .content("Bad-recommended")
+                                                        .responder(responder)
+                                                        .build();
         recommendationRepository.save(recommendation);
     }
 
     @Test
-    public void saveRecommendationTest2() {
-        // User requester = userRepository.findById(3L).orElseThrow();
-        User responder = userRepository.findById(4L).orElseThrow();
+    public void saveAllStudent() {
+        List<Recommendation> recommendationList = recommendationRepository.findAll();
+        System.out.println("Recommendation  : " +recommendationList);
+    }
+    
+    @Test
+    public void addRecommendationRequest() {
+        JobCreator responder = jobCreatorRepository.findById(5L).orElseThrow();
+        JobSeeker requester = jobSeekerRepository.findById(4L).orElseThrow();
 
-        Recommendation recommendation = Recommendation.builder()
-                                            .content("not-recommended")
-                                            .responder(responder)
-                                            .build();
-        
-        recommendationRepository.save(recommendation);
+        List<JobSeeker> recommendationRequest = responder.getRecommendationRequests();
+        if(recommendationRequest.contains(requester))
+            throw new DataIntegrityViolationException("Request already exitsts");
+        recommendationRequest.add(requester);
+        responder.setRecommendationRequests(recommendationRequest);
+        jobCreatorRepository.save(responder);
     }
 
     @Test
-    public void saveRecommendationTest3() {
-        // User requester = userRepository.findById(3L).orElseThrow();
-        User responder = userRepository.findById(1L).orElseThrow();
+    public void addRecommendation() {
+        JobCreator responder = jobCreatorRepository.findById(5L).orElseThrow();
+        JobSeeker requester = jobSeekerRepository.findById(4L).orElseThrow();
 
-        Recommendation recommendation = Recommendation.builder()
-                                            .content("weakly-recommended")
-                                            .responder(responder)
-                                            .build();
-        
-        recommendationRepository.save(recommendation);
+        List<JobSeeker> recommendationRequest = responder.getRecommendationRequests();
+        if(recommendationRequest.contains(requester)) {
+            List<Recommendation> requestRecommendationList = requester.getRecommendation();
+            Recommendation recommendation = Recommendation.builder()
+                                                .content("Highly-recommended")
+                                                .responder(responder)
+                                                .build();
+            recommendation = recommendationRepository.save(recommendation);
+
+            requestRecommendationList.add(recommendation);
+            requester.setRecommendation(requestRecommendationList);
+            jobSeekerRepository.save(requester);
+
+            recommendationRequest.remove(requester);
+            responder.setRecommendationRequests(recommendationRequest);
+            jobCreatorRepository.save(responder);
+
+        }else{
+            throw new NotFoundException("Request not found");
+        }
     }
 
-
     @Test
-    public void getRecommendationTest() {
+    public void getRecommendation() {
         Recommendation recommendation = recommendationRepository.findById(1L).orElseThrow();
         System.out.println(recommendation);
     }
 
     @Test
     public void updateRecommendation() {
-        Recommendation recommendation = recommendationRepository.findById(2L).orElseThrow();
+        Recommendation recommendation = recommendationRepository.findById(5L).orElseThrow();
         recommendation.setContent("highly recommended");
-
-        User requester = userRepository.findById(1L).orElseThrow();
-        recommendation.setRequester(requester);
-
-        recommendationRepository.save(recommendation);
+        Recommendation updatedRecommendation = recommendationRepository.save(recommendation);
+        System.out.println(updatedRecommendation);
     }
 
     @Test
     public void deleteRecommendation() {
-        Recommendation recommendation = recommendationRepository.findById(2L).orElseThrow();
+        Recommendation recommendation = recommendationRepository.findById(3L).orElseThrow();
         recommendationRepository.delete(recommendation);
 
     }
 
-    @Test
-    public void addRecommendationRequest() {
-        JobCreator responder = jobCreatorRepository.findById(7L).orElseThrow();
-        JobSeeker requester = jobSeekerRepository.findById(4L).orElseThrow();
-
-        List<JobSeeker> requestRecommendation = responder.getRequestRecommendation();
-        if (requestRecommendation.contains(requester))
-            throw new DataIntegrityViolationException("Request already exitsts");
-        requestRecommendation.add(requester);
-        responder.setRequestRecommendation(requestRecommendation);
-        jobCreatorRepository.save(responder);
-    }
-
-    @Test
-    public void addRecommendation() {
-        JobCreator responder = jobCreatorRepository.findById(7L).orElseThrow();
-        JobSeeker requester = jobSeekerRepository.findById(4L).orElseThrow();
-
-        List<JobSeeker> requestRecommendation = responder.getRequestRecommendation();
-        if(requestRecommendation.contains(requester)) {
-                List<Recommendation> requestRecommendationList = requester.getRecommendations();
-                Recommendation recommendation = Recommendation.builder()
-                                                    .content("Highly-recommended")
-                                                    .responder(responder)
-                                                    .build();
-                recommendation = recommendationRepository.save(recommendation);
-
-                requestRecommendationList.add(recommendation);
-                requester.setRecommendations(requestRecommendationList);
-                jobSeekerRepository.save(requester);
-
-                requestRecommendation.remove(requester);
-                responder.setRecommendationRequests(requestRecommendation);
-                jobCreatorRepository.save(responder);
-
-
-        }else{
-            throw new NotFoundException("Request not found");
-        }
-    }
-    
-    @Test
-    public void searchByJobSeeker() {
-        JobSeeker jobSeeker = jobSeekerRepository.findById(4L).orElseThrow();
-        List<Recommendation> searchRecommendationList = jobSeeker.getRecommendations();
-        System.out.println(searchRecommendationList);
-    }
-
-    
 }
