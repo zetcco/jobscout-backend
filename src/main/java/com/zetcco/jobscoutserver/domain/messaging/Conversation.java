4abf1458063422af706763fd19d39e5952b27c52
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-03-02 18:04:13 +0530

    Added builder for Conversation

--- src/main/java/com/zetcco/jobscoutserver/domain/messaging/Conversation.java
+++ src/main/java/com/zetcco/jobscoutserver/domain/messaging/Conversation.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -17,6 +18,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Conversation {
     
     @Id
