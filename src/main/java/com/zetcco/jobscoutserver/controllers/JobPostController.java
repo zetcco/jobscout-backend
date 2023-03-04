54ef4f3f5dd75ab9f1ac32347c8a10149a9740c4
ANMSilva <nipunmadumal18@gmail.com>
2023-03-04 14:00:44 +0530

    JobPostController modified

--- src/main/java/com/zetcco/jobscoutserver/controllers/JobPostController.java
+++ src/main/java/com/zetcco/jobscoutserver/controllers/JobPostController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.zetcco.jobscoutserver.domain.support.JobPostStatus;
@@ -46,7 +47,7 @@ public class JobPostController {
     }
 
     @GetMapping("/type")
-    public ResponseEntity<List<JobPostDTO>> getJobPostByType(@PathVariable("type") JobPostType type){
+    public ResponseEntity<List<JobPostDTO>> getJobPostByType(@RequestParam("val") JobPostType type){
         try{
             return new ResponseEntity<List<JobPostDTO>>(jobPostService.getJobPostByType(type) , HttpStatus.OK);
         } catch (Exception e){
@@ -55,13 +56,30 @@ public class JobPostController {
     }
  
     @GetMapping("/status")
-    public ResponseEntity<List<JobPostDTO>> getJobPostByStatus(@PathVariable("status") JobPostStatus status){
+    public ResponseEntity<List<JobPostDTO>> getJobPostByStatus(@RequestParam("val") JobPostStatus status){
         try{
             return new ResponseEntity<List<JobPostDTO>>(jobPostService.getJobPostByStatus(status) , HttpStatus.OK);
         } catch (Exception e){
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR , e.getMessage());
         }
     }
+
+    // @GetMapping("/filter")
+    // public ResponseEntity<List<JobPostDTO>> getJobPostByStatus(@RequestParam("by") String filterType, @RequestParam("value") ){
+    //     try{
+    //         switch (filterType) {
+    //             case "status":
+    //                 return new ResponseEntity<List<JobPostDTO>>(jobPostService.getJobPostByStatus(status) , HttpStatus.OK);
+    //             case "type":
+    //                 return new ResponseEntity<List<JobPostDTO>>(jobPostService.getJobPostByType(type) , HttpStatus.OK);
+    //             default:
+    //                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST , "Invalid parameters");
+    //         }
+    //         return new ResponseEntity<List<JobPostDTO>>(jobPostService.getJobPostByStatus(status) , HttpStatus.OK);
+    //     } catch (Exception e){
+    //         throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR , e.getMessage());
+    //     }
+    // }
 
     @PostMapping("/")
     public ResponseEntity<JobPostDTO> saveJobPost(@RequestBody JobPostDTO jobPostDTO){
@@ -85,9 +103,10 @@ public class JobPostController {
         }
 
     @DeleteMapping("/{jobPostId}")
-    public void deleteJobPostById(@PathVariable Long jobPostId){
+    public ResponseEntity<String> deleteJobPostById(@PathVariable Long jobPostId){
         try{
             jobPostService.deleteJobPostById(jobPostId);
+            return new ResponseEntity<String>("Success", HttpStatus.OK);
         }catch(Exception e){
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR , e.getMessage());
         }
