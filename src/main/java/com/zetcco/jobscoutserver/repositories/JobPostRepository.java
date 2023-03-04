761db9e4160952440542b8f040ef13b5cc09d424
ANMSilva <nipunmadumal18@gmail.com>
2023-03-04 10:20:19 +0530

    JobPostRepository.java created and modifed

--- src/main/java/com/zetcco/jobscoutserver/repositories/JobPostRepository.java
+++ src/main/java/com/zetcco/jobscoutserver/repositories/JobPostRepository.java
@@ -1,8 +1,19 @@
 package com.zetcco.jobscoutserver.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.zetcco.jobscoutserver.domain.JobPost;
+import com.zetcco.jobscoutserver.domain.support.JobPostStatus;
+import com.zetcco.jobscoutserver.domain.support.JobPostType;
 public interface JobPostRepository extends JpaRepository<JobPost, Long> {
     
+    List<JobPost> findByTitleContainingIgnoreCase(String name);
+
+    Optional<List<JobPost>> findByType(JobPostType type);
+
+    Optional<List<JobPost>> findByStatus(JobPostStatus status);
+
 }
