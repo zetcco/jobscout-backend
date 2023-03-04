49456c9bb97032cf17253e804e1d4b8a77be1b3c
ANMSilva <nipunmadumal18@gmail.com>
2023-03-04 10:26:15 +0530

    JobPostDTO.java created and modified

---
+++ src/main/java/com/zetcco/jobscoutserver/domain/support/dto/JobPostDTO.java
@@ -0,0 +1,31 @@
+package com.zetcco.jobscoutserver.domain.support.dto;
+
+import java.util.Date;
+import com.zetcco.jobscoutserver.domain.support.JobPostStatus;
+import com.zetcco.jobscoutserver.domain.support.JobPostType;
+import com.zetcco.jobscoutserver.services.support.ProfileDTO;
+import com.zetcco.jobscoutserver.domain.Category;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class JobPostDTO {
+
+    private Long Id;
+    private Date timestamp;
+    private Date dueDate;
+    private String title;
+    private String description;
+    private JobPostType type;
+    private Boolean urgent;
+    private JobPostStatus status;
+    private Category category;
+    private ProfileDTO profileDTO;
+    
+}
