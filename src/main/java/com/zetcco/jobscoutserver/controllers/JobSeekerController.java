c1e5dc443c472f4ce5893bc7a4e362b32b578c67
Thanis <77865201+thanis04@users.noreply.github.com>
2023-03-04 20:40:46 +0530

    return type and GetMapping changed

--- src/main/java/com/zetcco/jobscoutserver/controllers/JobSeekerController.java
+++ src/main/java/com/zetcco/jobscoutserver/controllers/JobSeekerController.java
@@ -1,31 +1,36 @@
 package com.zetcco.jobscoutserver.controllers;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.zetcco.jobscoutserver.domain.JobSeeker;
+import com.zetcco.jobscoutserver.domain.Skill;
 import com.zetcco.jobscoutserver.services.JobSeekerService;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/job-seeker")
 public class JobSeekerController {
 
     @Autowired
     private JobSeekerService jobSeekerService;
 
-    @GetMapping("/job-seeker")
-    public ResponseEntity<JobSeeker> getJobSeeker(@PathVariable Long categoryId, @PathVariable List<Long> skillId) {
+    @PutMapping("/update/skills/{categoryId}")
+    public ResponseEntity<List<Skill>> updateJobSeekerSkillsAndCategory(@PathVariable Long categoryId,
+            @RequestBody Map<String, List<Long>> request) {
 
         try {
-            return new ResponseEntity<JobSeeker>(jobSeekerService.getCategoryAndSkillListById(categoryId, skillId),
+            List<Long> participantIds = request.get("ids");
+            return new ResponseEntity<List<Skill>>(
+                    jobSeekerService.getCategoryAndSkillListById(categoryId, participantIds),
                     HttpStatus.OK);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
