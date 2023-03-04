0250a0914797b055dd3a3f7f0f626b98e57b88b1
ANMSilva <nipunmadumal18@gmail.com>
2023-03-04 10:23:17 +0530

    JobPost services test cases wrote and tested

---
+++ src/test/java/com/zetcco/jobscoutserver/services/JobPostServiceTest.java
@@ -0,0 +1,73 @@
+package com.zetcco.jobscoutserver.services;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.zetcco.jobscoutserver.domain.JobPost;
+import com.zetcco.jobscoutserver.domain.support.JobPostStatus;
+import com.zetcco.jobscoutserver.domain.support.JobPostType;
+import com.zetcco.jobscoutserver.domain.support.dto.JobPostDTO;
+import com.zetcco.jobscoutserver.repositories.JobPostRepository;
+import com.zetcco.jobscoutserver.services.mappers.JobPostMapper;
+
+@SpringBootTest
+public class JobPostServiceTest {
+
+    @Autowired
+    private JobPostService jobPostService;
+
+    @Autowired
+    private JobPostMapper mapper;
+
+    // @Autowired
+    // JobPostMapper mapper;
+
+    @Test
+    void testAddNewJobPost(){
+        Date newDate = new Date();
+        JobPost jobPost = JobPost.builder()
+                                .timestamp(newDate)
+                                .dueDate(newDate)
+                                .title("Fullstack developer")
+                                .description("part time job which you can work from anywhere")
+                                .type(JobPostType.TYPE_PART_TIME)
+                                .urgent(true)
+                                .status(JobPostStatus.STATUS_ACTIVE)
+                                .build();
+        jobPostService.addNewJobPost(this.mapper.mapToDto(jobPost));
+    }
+
+    @Test
+    void testUpdateJobPost(){
+        List<JobPostDTO> jobPost_1 = jobPostService.getAllJobPosts();
+        jobPost_1.forEach((p)->{System.out.println(p.getTitle());});
+
+        JobPostDTO exsistingJobPost = jobPostService.getJobPostById(1L);
+        exsistingJobPost.setTitle("Updated Title");
+        jobPostService.updateJobPost(1L , exsistingJobPost);
+
+        List<JobPostDTO> jobPost_2 = jobPostService.getAllJobPosts();
+        jobPost_2.forEach((p)->{System.out.println(p.getTitle());});
+    }
+
+    @Test
+    void testFindJobPostByType(){
+        List<JobPostDTO> jobPost = jobPostService.getJobPostByType(JobPostType.TYPE_PART_TIME);
+        jobPost.forEach((p)->{System.out.println(p.getTitle());});
+    }
+
+    @Test
+    void testFindJobPostByStatus(){
+        List<JobPostDTO> jobPost = jobPostService.getJobPostByStatus(JobPostStatus.STATUS_ACTIVE);
+        jobPost.forEach((p)->{System.out.println(p.getTitle());});
+    }
+  
+     @Test
+      void testDeleteJobPostById(){
+        JobPostDTO jobPostDTO = jobPostService.getJobPostById(3L);
+        jobPostService.deleteJobPostById(3L , jobPostDTO);
+      }
+ }
