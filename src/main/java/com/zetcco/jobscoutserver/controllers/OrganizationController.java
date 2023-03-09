49cdb69efbb739ff912ce72679f41431a41d70c4
Niroshan Sanjeewa <sanjeewa11138@usci.ruh.ac.lk>
2023-03-09 10:45:09 +0530

    OrganizationControllerJoinRequestfixs

--- src/main/java/com/zetcco/jobscoutserver/controllers/OrganizationController.java
+++ src/main/java/com/zetcco/jobscoutserver/controllers/OrganizationController.java
@@ -10,14 +10,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import com.zetcco.jobscoutserver.services.OrganizationService;
 import com.zetcco.jobscoutserver.services.support.NotFoundException;
 import com.zetcco.jobscoutserver.services.support.ProfileDTO;
@@ -60,10 +58,10 @@ public class OrganizationController {
         }
     }
 
-    @GetMapping("/{organizationId}")
-    public ResponseEntity<List<ProfileDTO>> fetchJobCreatorsRequest(@PathVariable Long organizationId) {
+    @GetMapping("/join-request")
+    public ResponseEntity<List<ProfileDTO>> fetchJobCreatorsRequest() {
         try {
-            return new ResponseEntity<List<ProfileDTO>>(organizationService.fetchJobCreatorsRequest(organizationId),
+            return new ResponseEntity<List<ProfileDTO>>(organizationService.fetchJobCreatorsRequest(),
                     HttpStatus.OK);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
