9cfc393adae680856cbc41854a52bd66fb2b2b9f
Gimhan Mahela <gimhanmahela@gmail.com>
2023-03-09 18:13:39 +0530

    create functonalities

--- src/test/java/com/zetcco/jobscoutserver/repositories/BlogPostRepositoryTest.java
+++ src/test/java/com/zetcco/jobscoutserver/repositories/BlogPostRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.zetcco.jobscoutserver.repositories;
 
 import java.util.Date;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import com.zetcco.jobscoutserver.domain.BlogPost;
 import com.zetcco.jobscoutserver.domain.support.User;
@@ -20,10 +23,10 @@ public class BlogPostRepositoryTest {
 
     @Test
     void saveBlogPost() {
-        User user = userRepository.findById(1L).orElseThrow();
+        User user = userRepository.findById(3L).orElseThrow();
         BlogPost blogPost = BlogPost.builder()
                 .user(user)
-                .content("WTF")
+                .content("post06")
                 .timeStamp(new Date())
                 .build();
 
@@ -32,9 +35,9 @@ public class BlogPostRepositoryTest {
 
     @Test
     void updateBlogPost() {
-        BlogPost blogPost = blogPostRepository.findById(2L).orElseThrow();
+        BlogPost blogPost = blogPostRepository.findById(4L).orElseThrow();
 
-        blogPost.setContent("WTF Updated");
+        blogPost.setContent("post03");
         blogPost.setTimeStamp(new Date());
 
         blogPostRepository.save(blogPost);
@@ -42,18 +45,58 @@ public class BlogPostRepositoryTest {
 
     @Test
     void deleteBlogPost() {
-        BlogPost blogPost = blogPostRepository.findById(2L).orElseThrow();
+        BlogPost blogPost = blogPostRepository.findById(3L).orElseThrow();
 
         blogPostRepository.delete(blogPost);
     }
 
     @Test
     void getBlogPost() {
-        BlogPost blogPost = blogPostRepository.findById(2L).orElseThrow();
+        BlogPost blogPost = blogPostRepository.findById(4L).orElseThrow();
 
         System.out.println("\n\n=============================================================\n\n");
         System.out.println(blogPost);
         System.out.println("\n\n=============================================================\n\n");
 
     }
+
+    @Test
+    void getAllBlogPost() {
+        Pageable page1 = PageRequest.of(0, 2);
+
+        List<BlogPost> blogPosts = blogPostRepository.findAll(page1).getContent();
+
+        System.out.println("\n\n=============================================================\n\n");
+
+        for (BlogPost blogPost : blogPosts) {
+            System.out.println(blogPost);
+        }
+
+        System.out.println("\n\n------------------------------n\n");
+
+        Pageable page2 = PageRequest.of(1, 2);
+
+        blogPosts = blogPostRepository.findAll(page2).getContent();
+
+        System.out.println("\n\n------------------------------n\n");
+
+        for (BlogPost blogPost : blogPosts) {
+            System.out.println(blogPost);
+        }
+
+        System.out.println("\n\n=============================================================\n\n");
+
+        /*
+         * System.out.println(
+         * "\n\n=============================================================\n\n");
+         * 
+         * for (BlogPost blogpost : blogPosts) {
+         * System.out.println(blogpost);
+         * 
+         * }
+         * System.out.println(
+         * "\n\n=============================================================\n\n");
+         */
+    }
+
 }
