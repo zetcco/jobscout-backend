37bcf53a1ce92bcb55be80eac3260e7bcdee3591
Gimhan Mahela <gimhanmahela@gmail.com>
2023-03-09 18:11:56 +0530

    create blogpost service

---
+++ src/main/java/com/zetcco/jobscoutserver/services/BlogPostService.java
@@ -0,0 +1,67 @@
+package com.zetcco.jobscoutserver.services;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.zetcco.jobscoutserver.domain.BlogPost;
+import com.zetcco.jobscoutserver.repositories.BlogPostRepository;
+
+@Service
+public class BlogPostService {
+
+    @Autowired
+    private BlogPostRepository blogPostRepository;
+
+    public List<BlogPost> getBlogPosts(int pageNo, int pageSize) {
+        Pageable page = PageRequest.of(pageNo, pageSize);
+        return blogPostRepository.findAll(page).getContent();
+    }
+
+    public BlogPost getBlogPost(Long id) {
+        BlogPost blogPost = blogPostRepository.findById(id).orElseThrow();
+        System.out.println("\n\n--------------------------------------------------------------\n\n");
+        System.out.println(blogPost);
+        System.out.println("\n\n--------------------------------------------------------------\n\n");
+        return blogPost;
+    }
+
+    public BlogPost updateBlogPost(Long id, String content, Date date) {
+        BlogPost blogPost = blogPostRepository.findById(id).orElseThrow();
+
+        blogPost.setContent(content);
+        blogPost.setTimeStamp(date);
+
+        System.out.println("\n\n--------------------------------------------------------------\n\n");
+        System.out.println(blogPost);
+        System.out.println("\n\n--------------------------------------------------------------\n\n");
+        return blogPostRepository.save(blogPost);
+    }
+
+    public BlogPost saveBlogPost(String content, Date date) {
+        BlogPost blogPost = new BlogPost();
+        blogPost.setContent(content);
+        blogPost.setTimeStamp(date);
+
+        System.out.println("\n\n--------------------------------------------------------------\n\n");
+        System.out.println(blogPost);
+        System.out.println("\n\n--------------------------------------------------------------\n\n");
+        return blogPostRepository.save(blogPost);
+    }
+
+    public BlogPost deleteBlogPost(Long id) {
+        BlogPost blogPost = blogPostRepository.findById(id).orElseThrow();
+
+        System.out.println("\n\n--------------------------------------------------------------\n\n");
+        System.out.println(blogPost);
+        System.out.println("\n\n--------------------------------------------------------------\n\n");
+
+        blogPostRepository.delete(blogPost);
+        return blogPost;
+    }
+
+}
