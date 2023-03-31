dfae98540c23367d2a5e0a09b0bbcd535ec745a7
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-03-31 13:10:08 +0530

    Get Programming languages

--- src/main/java/com/zetcco/jobscoutserver/controllers/CodeExecutorController.java
+++ src/main/java/com/zetcco/jobscoutserver/controllers/CodeExecutorController.java
@@ -1,9 +1,12 @@
 package com.zetcco.jobscoutserver.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.server.ResponseStatusException;
 import com.zetcco.jobscoutserver.services.CodeExecutorService;
 import com.zetcco.jobscoutserver.services.support.CodeExecutor.CodeSubmission;
 import com.zetcco.jobscoutserver.services.support.CodeExecutor.CodeSubmissionResult;
+import com.zetcco.jobscoutserver.services.support.CodeExecutor.ProgrammingLanguage;
 
 @Controller
 @RequestMapping("/code")
@@ -19,6 +23,16 @@ public class CodeExecutorController {
 
     @Autowired
     private CodeExecutorService executorService;
+
+    @GetMapping("/languages")
+    public ResponseEntity<List<ProgrammingLanguage>> getProgrammingLanguages() {
+        try {
+            return new ResponseEntity<List<ProgrammingLanguage>>(executorService.getLanguages(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
 
     @PostMapping("/submit")
     public ResponseEntity<CodeSubmissionResult> submitCode(@RequestBody CodeSubmission submission) {
--- src/main/java/com/zetcco/jobscoutserver/services/CodeExecutorService.java
+++ src/main/java/com/zetcco/jobscoutserver/services/CodeExecutorService.java
@@ -1,12 +1,16 @@
 package com.zetcco.jobscoutserver.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.zetcco.jobscoutserver.services.support.CodeExecutor.CodeSubmission;
 import com.zetcco.jobscoutserver.services.support.CodeExecutor.CodeSubmissionResult;
+import com.zetcco.jobscoutserver.services.support.CodeExecutor.ProgrammingLanguage;
 
 import reactor.core.publisher.Mono;
 
@@ -26,45 +30,20 @@ public class CodeExecutorService {
                                 .build();
     } 
 
-    public void getTest() {
-        this.client.get().uri("/about").exchangeToMono(response -> {
-            System.out.println("------------------------------");
-            System.out.println(response.statusCode());
-            System.out.println("------------------------------");
-            return response.bodyToMono(Void.class);
-        }).block();
-    }
-
-    public void postTest() {
-        CodeSubmission submission = new CodeSubmission(
-            "#include <stdio.h>\n\nint main(void) {\n  char name[10];\n  scanf(\"%s\", name);\n  printf(\"hello, %s\n\", name);\n  return 0;\n}",
-            4,
-            "motherfucker");
-
-        CodeSubmissionResult result = this.client.post()
-                    .uri("/submissions/?base64_encoded=true&wait=true")
-                    .body(Mono.just(submission), CodeSubmission.class)
-                    .exchangeToMono(response -> {
-                        System.out.println("--------------------------");
-                        System.out.println(response.statusCode());
-                        System.out.println("--------------------------");
-                        return response.bodyToMono(CodeSubmissionResult.class);
-                    })
-                    .block();
-
-        System.out.println(result);
-    }
-
     public CodeSubmissionResult submit(CodeSubmission submission) {
         return this.client.post()
                     .uri("/submissions/?base64_encoded=true&wait=true")
                     .body(Mono.just(submission), CodeSubmission.class)
-                    .exchangeToMono(response -> {
-                        System.out.println("--------------------------");
-                        System.out.println(response.statusCode());
-                        System.out.println("--------------------------");
-                        return response.bodyToMono(CodeSubmissionResult.class);
-                    })
+                    .retrieve()
+                    .bodyToMono(CodeSubmissionResult.class)
+                    .block();
+    }
+
+    public List<ProgrammingLanguage> getLanguages() {
+        return this.client.get()
+                    .uri("/languages")
+                    .retrieve()
+                    .bodyToMono(new ParameterizedTypeReference<List<ProgrammingLanguage>>() {})
                     .block();
     }
     
