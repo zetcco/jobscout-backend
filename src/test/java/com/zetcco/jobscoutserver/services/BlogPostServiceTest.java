222a89b990aeda7ffb28ff5b3e994432c5061f25
Gimhan Mahela <gimhanmahela@gmail.com>
2023-04-22 23:30:05 +0530

    completed

--- src/test/java/com/zetcco/jobscoutserver/services/BlogPostServiceTest.java
+++ src/test/java/com/zetcco/jobscoutserver/services/BlogPostServiceTest.java
@@ -1,77 +1,77 @@
-package com.zetcco.jobscoutserver.services;
+// package com.zetcco.jobscoutserver.services;
 
-import java.util.Date;
+// import java.util.Date;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-import com.zetcco.jobscoutserver.domain.BlogPost;
+// import com.zetcco.jobscoutserver.domain.BlogPost;
 
 
-@SpringBootTest
-public class BlogPostServiceTest {
+// @SpringBootTest
+// public class BlogPostServiceTest {
 
-    @Autowired
-    private BlogPostService blogPostService;
+//     @Autowired
+//     private BlogPostService blogPostService;
 
-    @Test
-    void getBlogPosts() {
-        for (BlogPost post : blogPostService.getBlogPosts(0, 2)) {
-            System.out.println(post);
-        }
+//     @Test
+//     void getBlogPosts() {
+//         for (BlogPost post : blogPostService.getBlogPosts(0, 2)) {
+//             System.out.println(post);
+//         }
 
-        System.out.println("---------------------");
+//         System.out.println("---------------------");
 
-        for (BlogPost post : blogPostService.getBlogPosts(1, 2)) {
-            System.out.println(post);
-        }
+//         for (BlogPost post : blogPostService.getBlogPosts(1, 2)) {
+//             System.out.println(post);
+//         }
 
-    }
+//     }
 
-    @Test
-    void getBlogPost() {
-        BlogPost blogPost = blogPostService.getBlogPost(2L);
-        System.out.println("\n\n--------------------------------------------------------------\n\n");
-        System.out.println(blogPost);
-        System.out.println("\n\n--------------------------------------------------------------\n\n");
+//     @Test
+//     void getBlogPost() {
+//         BlogPost blogPost = blogPostService.getBlogPost(2L);
+//         System.out.println("\n\n--------------------------------------------------------------\n\n");
+//         System.out.println(blogPost);
+//         System.out.println("\n\n--------------------------------------------------------------\n\n");
 
-    }
+//     }
 
-    @Test
-    void updateBlogPost() {
-        BlogPost blogPost = blogPostService.getBlogPost(3L);
+//     @Test
+//     void updateBlogPost() {
+//         BlogPost blogPost = blogPostService.getBlogPost(3L);
 
-        blogPost.setContent("postupdated");
-        blogPost.setTimeStamp(new Date());
+//         blogPost.setContent("postupdated");
+//         blogPost.setTimeStamp(new Date());
 
-        System.out.println("\n\n--------------------------------------------------------------\n\n");
-        System.out.println(blogPost);
-        System.out.println("\n\n--------------------------------------------------------------\n\n");
-        // blogPostService.saveBlogPost(BlogPost.getContent(), blogPost.getTimeStamp());
-        System.out.println(blogPost.getContent());
-        System.out.println(blogPost.getTimeStamp());
-        System.out.println("\n\n--------------------------------------------------------------\n\n");
-    }
+//         System.out.println("\n\n--------------------------------------------------------------\n\n");
+//         System.out.println(blogPost);
+//         System.out.println("\n\n--------------------------------------------------------------\n\n");
+//         // blogPostService.saveBlogPost(BlogPost.getContent(), blogPost.getTimeStamp());
+//         System.out.println(blogPost.getContent());
+//         System.out.println(blogPost.getTimeStamp());
+//         System.out.println("\n\n--------------------------------------------------------------\n\n");
+//     }
 
-    @Test
-    void saveBlogPost() {
-        BlogPost blogPost = new BlogPost();
-        blogPost.setContent("post011");
-        blogPost.setTimeStamp(new Date());
+//     @Test
+//     void saveBlogPost() {
+//         BlogPost blogPost = new BlogPost();
+//         blogPost.setContent("post011");
+//         blogPost.setTimeStamp(new Date());
 
-        System.out.println("\n\n--------------------------------------------------------------\n\n");
-        System.out.println(blogPost);
-        System.out.println("\n\n--------------------------------------------------------------\n\n");
-        // blogPostService.saveBlogPost(blogPost.getContent(), blogPost.getTimeStamp());
-    }
+//         System.out.println("\n\n--------------------------------------------------------------\n\n");
+//         System.out.println(blogPost);
+//         System.out.println("\n\n--------------------------------------------------------------\n\n");
+//         // blogPostService.saveBlogPost(blogPost.getContent(), blogPost.getTimeStamp());
+//     }
 
-    @Test
-    void deleteBlogPost() {
-        BlogPost blogPost = blogPostService.getBlogPost(2L);
-        blogPostService.deleteBlogPost(2L);
-        System.out.println("Deleted Post :" + blogPost);
+//     // @Test
+//     // void deleteBlogPost() {
+//     //     BlogPost blogPost = blogPostService.getBlogPost(2L);
+//     //     blogPostService.deleteBlogPost(2L);
+//     //     System.out.println("Deleted Post :" + blogPost);
 
-    }
-}
+//     // }
+// }
