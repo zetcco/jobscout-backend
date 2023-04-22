ba7e3dbff71809b00b0fae9ea680d6bd189b0ab1
Gimhan Mahela <gimhanmahela@gmail.com>
2023-04-22 10:57:34 +0530

    Change the url param into body

--- src/main/java/com/zetcco/jobscoutserver/controllers/BlogPostController.java
+++ src/main/java/com/zetcco/jobscoutserver/controllers/BlogPostController.java
@@ -1,20 +1,27 @@
 package com.zetcco.jobscoutserver.controllers;
 
+import java.nio.file.AccessDeniedException;
 import java.util.Date;
 import java.util.List;
 
+import org.hibernate.mapping.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.zetcco.jobscoutserver.domain.BlogPost;
 import com.zetcco.jobscoutserver.services.BlogPostService;
+import com.zetcco.jobscoutserver.services.support.BlogPostDTO;
 
 @Controller
 @RequestMapping("/posts")
@@ -44,34 +51,45 @@ public class BlogPostController {
         }
     }
 
-    @GetMapping("/update")
-    public ResponseEntity<BlogPost> updateBlogPost(Long id, String content, Date date) {
-
+    @PutMapping("/update")
+    public ResponseEntity<BlogPostDTO> updateBlogPost(@RequestBody BlogPostDTO newBlogPostDTO) {
         try {
-            BlogPost blogPost = blogPostService.getBlogPost(id);
-            System.out.println("Deleted Post :" + blogPost);
-            return new ResponseEntity<BlogPost>(blogPostService.saveBlogPost(content, date), HttpStatus.OK);
+            return new ResponseEntity<BlogPostDTO>(blogPostService.updateBlogPost(newBlogPostDTO), HttpStatus.OK);
+        } catch (AccessDeniedException e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage());
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
 
     }
 
-    @GetMapping("/delete")
-    public ResponseEntity<BlogPost> deleteBlogPost(Long id) {
-        try {
-            return new ResponseEntity<>(blogPostService.deleteBlogPost(id), HttpStatus.OK);
+    // @DeleteMapping("/delete")
+    // public ResponseEntity<BlogPostDTO> deleteBlogPost(@RequestBody Map<String,Long> deleteBlogPost)
+    //     try{
+    //         return new ResponseEntity<>(blogPostService.deleteBlogPost(deleteBlogPost), HttpStatus.OK);
+    //     } catch (Exception e) {
+    //         throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+    //     }
+
+    // }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<BlogPostDTO> deleteBlogPost(@RequestBody BlogPostDTO deleteBlogPostDTO)
+    {
+        try{
+            return new ResponseEntity<>(blogPostService.deleteBlogPost(deleteBlogPostDTO), HttpStatus.OK);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
 
     }
 
-    @GetMapping("/add")
-    public ResponseEntity<BlogPost> saveBlogPost(String content) {
+    @PostMapping("/add")
+    public ResponseEntity<BlogPostDTO> saveBlogPost(@RequestBody BlogPostDTO blogPostDTO) {
         try {
-            return new ResponseEntity<BlogPost>(blogPostService.saveBlogPost(content), HttpStatus.OK);
+            return new ResponseEntity<BlogPostDTO>(blogPostService.saveBlogPost(blogPostDTO), HttpStatus.OK);
         } catch (Exception e) {
+            e.printStackTrace();
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
 
