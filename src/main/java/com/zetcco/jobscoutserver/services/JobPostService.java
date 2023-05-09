fe0dc4e234e586d04ef14bc77c4ae8158708019a
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-05-10 03:01:54 +0530

    Refactors

--- src/main/java/com/zetcco/jobscoutserver/services/JobPostService.java
+++ src/main/java/com/zetcco/jobscoutserver/services/JobPostService.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
--- src/main/java/com/zetcco/jobscoutserver/services/support/ProfileDTO.java
+++ src/main/java/com/zetcco/jobscoutserver/services/support/ProfileDTO.java
@@ -2,7 +2,6 @@ package com.zetcco.jobscoutserver.services.support;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.zetcco.jobscoutserver.domain.support.Address;
 import com.zetcco.jobscoutserver.domain.support.Role;
 
 import lombok.AllArgsConstructor;
@@ -20,6 +19,5 @@ public class ProfileDTO {
     private String email;
     private Role role;
     private String displayName;
-    // private Address address;
     private String displayPicture;
 }
